c7068afd51f66c0ca92f5cd4b9ec84a606a345c7
ELIAS RADI BENJELLOUN <votre_mail>
2025-10-22 15:54:58 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eradi 1er programme destiner a afficher un message 
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " );
+        System.out.println( " au revoir " );
+        int i = 20;
+        String prenom; 
+Scanner sc ; 
+sc = new Scanner (System.in) ; 
+System.out.println("Quel est votre prénom ?"); 
+prenom = sc.nextLine();  
+    }
+    
+}
