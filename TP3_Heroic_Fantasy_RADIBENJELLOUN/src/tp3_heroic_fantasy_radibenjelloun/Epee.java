3067203134279c4bb41f705b4c59e84fbbe1fcac
ELIAS RADI BENJELLOUN <votre_mail>
2025-11-12 12:59:28 +0100

    TP3_Heroic_Fantasy_RADIBENJELLOUN

---
+++ TP3_Heroic_Fantasy_RADIBENJELLOUN/src/tp3_heroic_fantasy_radibenjelloun/Epee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_radibenjelloun;
+
+/**
+ *
+ * @author eradi
+ */
+public class Epee extends Arme {
+    private int indiceFinesse;
+
+    public Epee(String nom, int niveauAttaque,int indiceFinesse) {
+        super(nom, niveauAttaque);
+        if (indiceFi)
+    }
+}
