62c350204a25b3a615312ab2d5657c73caae8706
ELIAS RADI BENJELLOUN <votre_mail>
2025-12-05 15:28:01 +0100

    TP_interface_Elias

---
+++ miniprojet_cadenas_RADIBENJELLOUN/src/Resultat.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eradi
+ */
+class Resultat {
+
+    Resultat(int exact, int tropHaut, int tropBas) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int getExact() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
